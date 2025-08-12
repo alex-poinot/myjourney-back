01c8a6c7171dc47830ad62362099edd9a70d8b59
alex-poinot <77551076+alex-poinot@users.noreply.github.com>
2025-08-12 15:04:00 +0200

    Correction de la configuration Jest pour les modules ES

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 export default {
   testEnvironment: 'node',
-  transform: {},
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
-  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
   },
